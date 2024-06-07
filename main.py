bf5c214c6ba11aca93e4d4a1e9be657f49aebeeb
Deivinsom <deivid.oliveira00000@gmail.com>
2024-06-07 16:22:07 -0300

    Terminado, eu acho

--- main.py
+++ main.py
@@ -27,6 +27,49 @@ def create_square_grid():
         grid.append(linha)
     return grid
 
+# lidando com o clique do jogador
+def square_clicked(linha,coluna):
+    quadrado = quadrados[linha][coluna]
+    cor = quadrado['bg']
+    if cor=='black':
+        quadrado['bg'] = grid[linha][coluna]
+        quadrado_mostrado.append(quadrado)
+        if len(quadrado_mostrado) == 2:
+            check_match()
+
+
+# verificando se os quadrados sao iguais
+def check_match():
+    quadrado1, quadrado2 = quadrado_mostrado
+    if quadrado1['bg'] == quadrado2['bg']:
+        quadrado1.after(1000, quadrado1.destroy)
+        quadrado2.after(1000, quadrado2.destroy)
+        quadrado_correspondente.extend([quadrado1, quadrado2])
+        check_win()
+    else:
+        quadrado1.after(1000, lambda:quadrado1.config(bg='black'))
+        quadrado2.after(1000, lambda:quadrado2.config(bg='black'))
+    quadrado_mostrado.clear()
+    uptade_score()
+
+
+# verificando se o jogador ja ganhou
+def check_win():
+    if len(quadrado_correspondente) == NUM_LINHAS * NUM_COLUNAS:
+        messagebox.showinfo("Parabéns!", "Voce ganhou o jogo!")
+        janela.quit()
+
+
+# atualiza a pontuação e verifica se o jogador perdeu
+def uptade_score():
+    global numero_tentativas
+    numero_tentativas += 1
+    label_tentativas.config(text='Tentativas: {}/{}'.format(numero_tentativas, MAX_TENTATIVAS))
+    if numero_tentativas >= MAX_TENTATIVAS:
+        messagebox.showinfo("Fim de Jogo", "Voce perdeu!")
+        janela.quit()
+    
+
 # criando a interface principal
 janela = tk.Tk()
 janela.title('Jogo da Memória')
@@ -42,7 +85,7 @@ numero_tentativas = 0
 for linha in range(NUM_LINHAS):
     linha_quadrados = []
     for coluna in range(NUM_COLUNAS):
-        quadrado = tk.Button(janela, width=QUADRADO_SIZE_WIDTH, height=QUADRADO_SIZE_HEIGHT, bg='black', relief=tk.RAISED, bd=3)
+        quadrado = tk.Button(janela, command=lambda r=linha, c=coluna:square_clicked(r,c), width=QUADRADO_SIZE_WIDTH, height=QUADRADO_SIZE_HEIGHT, bg='black', relief=tk.RAISED, bd=3)
         quadrado.grid(row=linha, column=coluna, padx=5, pady=5)
         linha_quadrados.append(quadrado)
     quadrados.append(linha_quadrados)
@@ -53,7 +96,7 @@ button_style = {'activebackground': '#f8f9fa', 'font':FONT_STYLE, 'fg':COR_LETRA
 janela.option_add('*Button', button_style)
 
 # label para o numero de tentativas
-label_tentativas = tk.Label(janela, text='Tentativas: {}/{}'.format(9,MAX_TENTATIVAS), fg=COR_LETRA, bg=COR_FUNDO, font=FONT_STYLE)
+label_tentativas = tk.Label(janela, text='Tentativas: {}/{}'.format(numero_tentativas, MAX_TENTATIVAS), fg=COR_LETRA, bg=COR_FUNDO, font=FONT_STYLE)
 label_tentativas.grid(row=NUM_LINHAS, columnspan=NUM_COLUNAS, padx=10, pady=10)
 
 
